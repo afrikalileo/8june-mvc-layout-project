78e8db52f5e84fdc6546093ebcd12779947cd0cc
afrikalileo <veyseltekin1683@gmail.com>
2022-06-08 21:06:49 +0300

    final commit

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+
+        {
+
+            return View();
+        }
+
+        public IActionResult Product()
+        {
+            return View();
+        }
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Store()
+        {
+            return View();
+        }
+    }
+}
